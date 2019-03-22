8c6d28991498fca41f78f49014a5705a1a1fb607
Michael Bleier <michael.bleier@uni-wuerzburg.de>
2019-03-22 23:20:18 +0100

    scout3d_motor: send raw encoder.

--- scout3d_motor/src/motorencoder_node.py
+++ scout3d_motor/src/motorencoder_node.py
@@ -3,7 +3,7 @@
 import rospy
 import serial
 import threading
-from std_msgs.msg import UInt16
+from sensor_msgs.msg import JointState
 
 def publishEncoderValues():
     direction = 0 # motor direction -1 and +1
@@ -12,18 +12,32 @@ def publishEncoderValues():
     while ser.readable() and not rospy.is_shutdown():
         data = ser.readline()
         ser.flushInput()
-        datalist = data.split(',')
+        datalist = data.split(' ')
+        packet = ''
         value = 0
-        deg = 0.0
-        if len(datalist) == 3:
+        status = 0
+        button = 0
+        counter = 0
+        if len(datalist) == 5:
             try:
-                value = int(datalist[0])
-                deg = float(datalist[2])
+                packet = datalist[0]
+                value = int(datalist[1])
+                status = int(datalist[2])
+                button = int(datalist[3])
+                counter = int(datalist[4])
             except ValueError:
                 continue
-            if 4095 >= value >= 0 and 360 >= deg >= 0:
-                pub.publish(value)
+            if 4096 >= value >= 0:
+                msg = JointState()
+                msg.header.stamp = rospy.Time.now()
+                msg.header.frame_id = "/encoder_raw"
+                msg.name = [ "encoder_joint" ]
+                msg.position = [ value ]
+                msg.velocity = [ 0 ]
+                msg.effort = [ 0 ]
+                pub.publish(msg)
                 print(str(value))
+                
                 lasttickvalue = value
                 if direction > 0:
                     diff = 0
@@ -40,10 +54,9 @@ def publishEncoderValues():
                         diff = lasttickvalue - value
                     totalticks -= diff
 
-
 if __name__ == '__main__':
     rospy.init_node('motorencoder_node', anonymous=True)
-    pub = rospy.Publisher('encoder_raw', UInt16, queue_size=10)
+    pub = rospy.Publisher('encoder_raw', JointState, queue_size=10)
     try:
         ser = serial.Serial('/dev/ttyS0', 115200)
         threading.Thread(target=publishEncoderValues).start()
