308d428257675c0bfc6d8f046cd38f0f2ecadec5
Michael Bleier <michael.bleier@uni-wuerzburg.de>
2019-04-02 15:51:57 +0200

    scout3d_motor: publish stopped.

--- scout3d_motor/src/motor_node.py
+++ scout3d_motor/src/motor_node.py
@@ -5,6 +5,7 @@ import time
 import threading
 import subprocess
 import math
+from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 from sensor_msgs.msg import JointState
 from scout3d_motor.srv import MotorPositionCommand
@@ -36,28 +37,36 @@ def updatePosition():
         msg.position = [ positionRad ]
         msg.velocity = [ 0 ]
         msg.effort = [ 0 ]
-        pub.publish(msg)
+        pubPosition.publish(msg)
         # print('position: ' + str(positionDeg) + ' (' + str(position) + ')')
 
-        if not math.isnan(setpoint):
-            if setpoint != position:
-                # print('driving: ' + str(setpoint))
-                subprocess.check_output(["/home/scout3d/setMotorPosition", str(setpoint)])
-            else:
-                # print('deenergize')
-                subprocess.check_output(["/home/scout3d/off", ""])
+        if not math.isnan(setpoint) and setpoint != position:
+            # print('driving: ' + str(setpoint))
+            subprocess.check_output(["/home/scout3d/setMotorPosition", str(setpoint)])
+
+            msg = Bool()
+            msg.data = False
+            pubStopped.publish(msg)
+        else:
+            # print('deenergize')
+            subprocess.check_output(["/home/scout3d/off", ""])
+
+            msg = Bool()
+            msg.data = True
+            pubStopped.publish(msg)
+
         time.sleep(.01)
 
 if __name__ == '__main__':
     global setpoint
     setpoint = float('nan')
     rospy.init_node('motor_node', anonymous=True)
-    pub = rospy.Publisher('motorPosition', JointState, queue_size=10)
+    pubPosition = rospy.Publisher('motorPosition', JointState, queue_size=10)
+    pubStopped = rospy.Publisher('motorStopped', Bool, queue_size=10)
     serPos = rospy.Service('setMotorPosition', MotorPositionCommand, setPosition)
     serZero = rospy.Service('setMotorZero', MotorPositionCommand, setZero)
     try:
         threading.Thread(target=updatePosition).start()
         rospy.spin()
-        ser.close()
     except rospy.ROSInterruptException:
         pass
